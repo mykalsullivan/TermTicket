41cd64e6e9e1b5f5d96d9cded023f38c5c5b2002
Mykal Sullivan <80817911+mykalsullivan@users.noreply.github.com>
2024-11-18 16:08:46 -0600

    Set up some semblance of a client program

--- src/Client.cpp
+++ src/Client.cpp
@@ -3,58 +3,76 @@
 //
 
 #include "Client.h"
+#include "TicketManager.h"
 #include <string>
 #include <bits/getopt_core.h>
 
-Client::Client(int argc, char *argv[])
+void printHelpMenu()
 {
-    std::string username;
-    std::string password;
-    std::string hostname;
-    int port;
 
-    while (c = getopt(argc, argv, "u:w:h:p:"))
-    {
-        switch (c)
-        {
-            case 'u':
+}
 
-                break;
-            case 'p':
-
-                break;
-            case 'h':
-
-                break;
-            case 'P':
-
-                break;
-            case '?':
-            case 'h':
-
-                break;
-            default:
-                break;
-        }
-    }
-
-
+Client::Client(int argc, char **argv)
+{
+    m_Args.argc = argc;
+    m_Args.argv = argv;
 }
 
 Client::~Client()
 {
-
+    if (m_TicketManager != nullptr)
+        delete m_TicketManager;
 }
 
 int Client::run()
 {
+    /* 1. Set up connection */
+    m_TicketManager = setupTicketManager();
 
+    /* 2. */
+    while (m_Running)
+    {
 
+    }
 
     return 0;
 }
 
 void Client::exit()
 {
+    m_Running = false;
+}
 
+TicketManager *Client::setupTicketManager()
+{
+    std::string username;
+    std::string password;
+    std::string hostname;
+    int port;
+
+    while (int c = getopt(m_Args.argc, m_Args.argv, "u:w:h:p:"))
+    {
+        switch (c)
+        {
+            case 'u':
+                username = optarg;
+            break;
+            case 'p':
+                password = optarg;
+            break;
+            case 'H':
+                hostname = optarg;
+            break;
+            case 'P':
+                port = std::atoi(optarg);
+            break;
+            case '?':
+            case 'h':
+                printHelpMenu();
+            return nullptr;
+            default:
+                printHelpMenu();
+            return nullptr;
+        }
+    }
 }
--- src/Client.h
+++ src/Client.h
@@ -4,21 +4,37 @@
 
 #pragma once
 #include <string>
+#include <cassert>
 #include <pqxx/pqxx>
 
 // Forward declaration(s)
 class TicketManager;
 
+struct CommandLineArgs {
+    int argc = 0;
+    char **argv;
+
+    const char *operator[](int n) const
+    {
+        assert(n >= 0 && n < argc && "Out of bounds");
+        return argv[n];
+    }
+};
+
 class Client {
 public:
-    Client(int argc, char *argv[]);
+    Client(int argc, char **argv);
     ~Client();
 
 private:
     bool m_Running = false;
+    CommandLineArgs m_Args;
     TicketManager *m_TicketManager;
 
 public:
     int run();
     void exit();
+
+private:
+    TicketManager *setupTicketManager();
 };
